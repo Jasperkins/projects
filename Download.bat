1828d2507b8368f0c3cb92ad9383a5dd21e60a35
Jasperkins <55040732+Jasperkins@users.noreply.github.com>
2023-05-15 21:03:14 -0500

    Create Download.bat

---
+++ Download.bat
@@ -0,0 +1,10 @@
+@echo off
+
+echo Downloading DISA STIG Viewer
+
+curl "https://dl.dod.cyber.mil/wp-content/uploads/stigs/zip/U_STIGViewer_2-17_Win64.zip" --output STIGviewer.zip
+
+echo Download Done
+
+
+pause
